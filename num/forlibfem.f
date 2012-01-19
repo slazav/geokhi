73050f16e0b45fe5211fae1f39e2f8bb7a2adf5b
Vladislav Zavjalov <slazav@altlinux.org>
2012-01-19 13:37:19 +0400

    update

---
+++ num/forlibfem.f
@@ -0,0 +1,233 @@
+C ======================================================================
+      Subroutine FEM2Dext(XY1, XY2, XY3, 
+     &           lbE, lbF, lbP, dDATA, iDATA, iSYS,
+     &           LDA, A, F, nRow, nCol,
+     &           templateR, templateC)
+C ======================================================================
+      Implicit none
+      Include 'fem2Dtri.fd'
+      Include 'assemble.fd'
+C ======================================================================
+      Real*8  XY1(*), XY2(*), XY3(*)
+      
+      Integer lbE, lbF(3), lbP(3)
+      Real*8  dDATA(*)
+      Integer iDATA(*), iSYS(*), LDA, nRow, nCol
+
+      Real*8  A(LDA, *), F(*)
+      Integer templateR(*), templateC(*)
+
+C Local variables
+      Integer  Ddiff, Drhs, Dbc
+      External Ddiff, Drhs, Dbc
+
+      Integer  i,k, ir,ic, label, ibc
+      Real*8   x, y, eBC(1)
+      Logical  ifXbc
+
+C ======================================================================
+      nRow = 3
+      nCol = 3
+
+c ... set up templates 
+      Do i = 1, 3
+         templateR(i) = Vdof
+         templateC(i) = Vdof
+      End do
+
+c ... compute the stiffness matrix A
+      Call fem2Dtri(XY1, XY2, XY3,
+     &              GRAD, FEM_P1, GRAD, FEM_P1,
+     &              label, Ddiff, dDATA, iDATA, iSYS, 1,
+     &              LDA, A, ir, ic)
+
+c ... compute right hand side F
+      Call fem2Dtri(XY1, XY2, XY3,
+     &              IDEN, FEM_P0, IDEN, FEM_P1,
+     &              lbE, Drhs, dDATA, iDATA, iSYS, 2,
+     &              1, F, ir, ic)
+
+c ... impose boundary conditions (assume nRow = nCol)
+      Do k = 1, 3
+         If(lbP(k).GT.0) Then
+            If(k.EQ.1) Then
+               x = XY1(1)
+               y = XY1(2)
+            ElseIf(k.EQ.2) Then
+               x = XY2(1)
+               y = XY2(2) 
+            ElseIf(k.EQ.3) Then
+               x = XY3(1)
+               y = XY3(2)
+            End if
+
+            ibc = Dbc(x, y, lbP(k), dDATA, iDATA, iSYS, eBC)
+
+            If(ifXbc(ibc, BC_DIRICHLET)) Then
+               Call applyDIR(LDA, nRow, A, F, k, eBC)
+            End if
+         End if
+      End do
+
+      Return
+      End
+
+
+
+C ======================================================================
+C  Diffusion tensor             
+C ======================================================================
+      Integer Function Ddiff(x, y, label, dDATA, iDATA, iSYS, Coef)
+      include 'fem2Dtri.fd'
+
+      Real*8  dDATA(*), x, y, Coef(MaxTensorSize, 4)
+      Integer iDATA(*), label, iSYS(*)
+
+      iSYS(1) = 1
+      iSYS(2) = 1
+
+      Coef(1,1) = 1D0
+
+      Ddiff = TENSOR_NULL
+
+      Return
+      End
+
+
+
+C ======================================================================
+C Boundary condition
+C ======================================================================
+      Integer Function Dbc(x, y, label, dDATA, iDATA, iSYS, eBC)
+      Include 'fem2Dtri.fd'
+
+      Real*8  dDATA(*), x, y, eBC(MaxTensorSize, *)
+      Integer iDATA(*), label, iSYS(*)
+
+      iSYS(1) = 1
+      iSYS(2) = 1
+
+      If (label.EQ.1) Then
+         Dbc = BC_NEUMANN
+         eBC(1,1) = 0D0
+      Else If (label.EQ.2) Then
+         Dbc = BC_DIRICHLET
+         eBC(1,1) = 1D0
+      Else If (label.EQ.3) Then
+         Dbc = BC_DIRICHLET
+         eBC(1,1) = 2D0
+      Else
+         Write(*,*) 'Dbc: wrong label=', label
+         Stop
+      End if
+
+      Return
+      End
+
+
+
+C ======================================================================
+C Right hand side
+C ======================================================================
+      Integer Function Drhs(x, y, label, dDATA, iDATA, iSYS, F)
+      Include 'fem2Dtri.fd'
+
+      Real*8  dDATA(*), x, y, F(MaxTensorSize, *)
+      Integer iDATA(*), label, iSYS(*)
+
+      iSYS(1) = 1
+      iSYS(2) = 1
+
+      F(1, 1) = 0D0
+      Drhs = TENSOR_SCALAR
+
+      Return
+      End
+
+
+
+c ======================================================================
+c  Exact (analytical) solution
+c ======================================================================
+      Integer Function DexactU(x, y, label, dDATA, iDATA, iSYS, U)
+      Include 'fem2Dtri.fd'
+
+      Real*8   dDATA(*), x, y, U(MaxTensorSize, *)
+      Integer  iDATA(*), label, iSYS(*)
+
+      Real*8   phi, r, pi
+
+c ======================================================================
+      iSYS(1) = 1
+      iSYS(2) = 1
+
+      pi = 4d0*datan(1d0)
+
+      If(x.gt.1d-10) Then
+         phi = datan(y/x)
+         If(phi.lt.0) phi = phi+2*pi
+
+      Else If(x.lt.-1d-10) Then
+         phi = datan(y/dabs(x))
+         phi = pi - phi
+
+      Else
+         If(y.gt.0) phi = pi/2
+         If(y.le.0) phi = 3*pi/2
+      End if
+
+      r = dsqrt(x**2 + y**2)
+      U(1,1) = r**0.25d0 * dsin(phi/4)
+
+      DexactU = TENSOR_SCALAR
+
+      Return
+      End
+
+
+
+c ======================================================================
+c  Exact (analytical) gradient of the solution
+c ======================================================================
+      Integer Function DgradU(x, y, label, dDATA, iDATA, iSYS, U)
+      implicit none
+      Include 'fem2Dtri.fd'
+
+      Real*8    dDATA(*), x, y, U(MaxTensorSize, *)
+      Integer   iDATA(*), label, iSYS(*)
+        
+      Integer   DexactU, i
+      EXTERNAL  DExactU
+
+      Real*8    delta, xD, yD, u0(1), uD(1)
+      PARAMETER(delta = 1d-9)
+
+c ==========================================================
+      xD = x + delta
+
+      i = DexactU(x,  y, label, dDATA, iDATA, iSYS, u0)
+      i = DexactU(xD, y, label, dDATA, iDATA, iSYS, uD)
+
+      U(1,1) = (uD(1) - u0(1)) / delta
+
+      If(y.gt.0D0) Then
+         yD = y + delta
+         i  = DexactU(x, yD, label, dDATA, iDATA, iSYS, uD)
+         U(2,1) = (uD(1) - u0(1)) / delta
+      Else
+         yD = y - delta
+         i  = DexactU(x, yD, label, dDATA, iDATA, iSYS, uD)
+         U(2,1) = -(uD(1) - u0(1)) / delta
+      End if
+
+      iSYS(1) = 2
+      iSYS(2) = 1
+      DgradU = TENSOR_GENERAL
+
+      Return
+      End
+
+
+
+
+
---
+++ num/main.f
@@ -0,0 +1,299 @@
+c ======================================================================
+      Program Stokes
+c ======================================================================
+c The adaptive solution for the following bvp:
+c
+c  -div grad u  + grad p = 0   in Omega
+c        div u           = 0   in Omega
+c
+c                      u = u_0 on dOmega_1
+c                      u = 0   on dOmega_2
+c                  du/dn = 0   on dOmega_3
+c
+c where Omega is a domain with a circle hole,  dOmega_1 is the side 
+c at x=-2, dOmega_3 is the side at x=2, and dOmega_2 is the rest of 
+c the boundary. The non-homogeneous boundary condition is 
+c
+c    u_0 = { 1-4y^2), 0 }.
+c
+c We use the P2 finite elements for the velocity u and P1 finite elements 
+c for the pressure p, which are known to be stable. The discretization 
+c method results in a symmetric indefinite matrix.
+c
+c ======================================================================
+      implicit none
+
+c nvmax - maximum number of mesh nodes
+c ntmax - maximum number of mesh triangles
+c nbmax - maximum number of boundary edges
+c namax - maximum number of non-zero matrix entries
+      Integer   nvmax,ntmax,nbmax,nfmax,namax
+      parameter(nvmax = 50 000, ntmax = 2*nvmax, nbmax = 5 000)
+      parameter(nfmax = 200 000, namax = 2 000 000)
+
+c work memory
+      Integer   MaxWr, MaxWi
+      Parameter(MaxWr = 4 000 000, MaxWi = 6 000 000)
+
+      Integer  iW(MaxWi)
+      Real*8   rW(MaxWr)
+      Real     ANI_CrvFunction
+
+c ======================================================================
+c Mesh definition
+c ======================================================================
+
+c mesh generator data specifying domain via in the segment format
+      double precision vbr(2,nbmax)
+      integer          Nbr
+
+      double precision vrt(2,nvmax)
+      integer          tri(3,ntmax), bnd(2,nbmax)
+c ... AFT2D library function
+      Integer   aft2dfront
+      EXTERNAL  aft2dfront
+
+      Integer  nv, nvfix, labelV(nvmax), fixedV(1)
+      Integer  nb, nbfix, labelB(nbmax), fixedB(1)
+      integer  nt, ntfix, labelT(ntmax), fixedT(1)
+      Integer  nc, labelC(nbmax)
+      Real*8   crv(2,nbmax)
+
+      DATA     nvfix/0/, fixedV/0/,  nbfix/0/, ntfix/0/, nc/0/
+
+
+c ======================================================================
+c for library aniFEM
+c ======================================================================
+      include 'fem2Dtri.fd'
+      include 'assemble.fd'
+
+      Integer  IA(nfmax), JA(namax)
+      Real*8    A(namax), RHS(nfmax), SOL(nfmax), RES(nfmax)
+
+      Integer  iDATAFEM(1), iSYS(MAXiSYS), controlFEM(3)
+      Real*8   dDATAFEM(1)
+
+      Integer  Dbc 
+      EXTERNAL Dbc, FEM2Dext
+
+
+c ======================================================================
+c for library aniLU
+c ======================================================================
+      Integer  symbolic(2), numeric(2), sys
+      Real*8   lucontrol(20), luinfo(90)
+
+
+c ======================================================================
+c for library aniLMR
+c ======================================================================
+      Real*8   Lp
+      Real*8   Metric(3,nvmax)
+
+
+c ======================================================================
+c for library aniMBA
+c ======================================================================
+      Integer  control(6), nEStar
+      Real*8   Quality
+      EXTERNAL CrvFunction_user
+
+
+c LOCAL VARIABLEs
+      Integer  i, j, dummy, iLoop, nLOOPs, nRow, nCol, iERR
+      Integer  nr, ibc, iv1,iv2
+      Real*8   rmax, h, x, y, eBC(2)
+      Character*30 file_mesh, file_velocity
+
+c ======================================================================
+c number of adaptive loops
+      nLOOPs = 5
+
+C Read input file that contains coordinates of boundary points
+      Open(1,file='./cell.txt')
+         Read(1,*) Nbr
+         Do i = 1, Nbr
+            Read(1,*) (vbr(j,i),j=1,2)
+         End do
+      Close(1)
+
+C Generate a mesh  starting  from boundary mesh
+      ierr=aft2dfront(
+     &           0, dummy, Nbr, vbr,   ! segment data
+     &           nv, vrt,              ! mesh data on output
+     &           nt, tri, labelT,
+     &           nb, bnd, labelB)
+      If (ierr.ne.0) stop ' error in function aft2dfront'
+      Write(*,5000) Nbr, nt, nv
+
+      labelb(2)=2
+      labelb(6)=3
+
+
+c begin adaptive iterative loop
+      Do iLoop = 1, nLOOPs
+         Write(*,'(/,A,I2)') '===> LOOP: ', iLoop
+
+c === no data is provided for the user subroutine Ddiff
+         dDATAFEM(1) = 0D0
+         iDATAFEM(1) = 0
+
+c mark the Dirichlet points with the maximal edge color
+         Call markDIR(nv, vrt, labelV, nb, bnd, labelB, 
+     &                Dbc, dDATAFEM, iDATAFEM, iSYS)
+
+C      write (*,*) 'VRT:'
+C      Do i=1, nv
+C        write (*,*) vrt(i,1), vrt(i,2), labelV(i)
+C      End do
+
+      write (*,*) 'BRD:'
+      Do i=1, nb
+        write (*,*) bnd(i,1), bnd(i,2), labelB(i)
+      End do
+
+C      write (*,*) 'TRI:'
+C      Do i=1, nt
+C        write (*,*) tri(i,1), tri(i,2), tri(i,3), labelT(i)
+C      End do
+
+
+c === general sparse matrix in a 0-based CSC format used in UMFPACK
+         controlFEM(1) = IOR(MATRIX_GENERAL, FORMAT_CSC)
+         controlFEM(2) = 1
+
+         Call BilinearFormTemplate(
+     &        nv, nb, nt, vrt, labelV, bnd, labelB, tri, labelT,
+     &        FEM2Dext, dDATAFEM, iDATAFEM, controlFEM,
+     &        nfmax, namax, IA, JA, A, RHS, nRow, nCol,
+     &        MaxWi, MaxWr, iW, rW)
+
+
+c  ===   call the driver for LU factorization and solution
+         Call CSC2CSC0(nCol, IA, JA)
+
+c set up default control parameters & print only error messages
+         Call umf4def(lucontrol)
+         lucontrol(1) = 1
+
+c pre-order and symbolic analysis
+         Call umf4sym(nCol, nCol, IA,JA,A, symbolic,lucontrol,luinfo)
+         If(luinfo(1).LT.0) Goto 5001
+
+c numeric factorization
+         Call umf4num(IA,JA,A, symbolic,numeric,lucontrol,luinfo)
+         If(luinfo(1).LT.0) Goto 5002
+
+c free the symbolic analysis data
+         Call umf4fsym(symbolic)
+
+c solve Ax=b, without iterative refinement
+         sys = 0
+         Call umf4sol(sys, SOL, RHS, numeric, lucontrol,luinfo)
+         If(luinfo(1).LT.0) Goto 5003
+
+c free the numeric factorization data
+         Call umf4fnum (numeric)
+
+
+c check the residual
+         Call mulAcsc0(nRow, IA, JA, A, SOL, RES)
+         rmax = 0
+         Do i = 1, nRow
+            rmax = max(rmax, RES(i) - RHS(i))
+         End do
+         Write(*,'(A,E12.6)') 'LU:  maximal norm of residual: ', rmax
+
+
+c === draw mesh and solution isolines as ps-figures (mid-edges are ignored)
+         nr  = (nRow - 3*nv) / 2
+
+c  PostScript file names must have extension .ps
+c  demo graphics has been activated
+c  Isolines of velocity components can be done with the following calls:
+c  Call isolines(SOL(iux), nv,vrt, nt,tri, nb,bnd,'velocity_x.ps',20)
+c  Call isolines(SOL(iuy), nv,vrt, nt,tri, nb,bnd,'velocity_y.ps',20)
+         If(iLoop.EQ.1) Then
+            Call isolines_demo(SOL(1), nv,vrt, nt,tri, nb,bnd,
+     &           'streamlines_ini.ps', 20, '')
+            Call graph_demo(nv,vrt, nt,tri,
+     &           'mesh_ini.ps', 'Initial quasi-uniform mesh')
+            Call draw_matrix(nRow, IA, JA, 'matrix_ini.ps')
+         Else
+            file_mesh     = 'mesh_final.ps'
+            file_velocity = 'streamlines_fin.ps'
+            Call isolines_demo(SOL(1), nv,vrt, nt,tri, nb,bnd,
+     &           'streamlines_fin.ps', 40, '')
+            Call graph_demo(nv,vrt, nt,tri,
+     &           'mesh_final.ps', 'final mesh')
+            Call draw_matrix(nRow, IA, JA, "matrix_fin.ps")
+         End if
+
+         If(iLoop.eq.nLOOPs) Goto 500
+
+
+c  ===   generate metric (from SOL) optimal for the L_p norm
+c        Lp = 0             ! maximum norm
+         Lp = 1             ! L_1 norm
+         Call Nodal2MetricVAR(SOL(1),
+     &                        vrt, nv, tri, nt, bnd, nb, Metric,
+     &                        MaxWr, rW, MaxWi, iW)
+
+         If(Lp.GT.0) Call Lp_norm(nv, Lp, Metric)
+
+
+c === generate the adaptive mesh to u_x
+         nEStar = 20000
+         control(1) = nEStar/10  ! MaxSkipE
+         control(2) = nEStar*10  ! MaxQItr
+         control(3) = 16+1    ! status = forbid boundary triangles (see aniMBA/status.fd)
+         control(4) = 1       ! flagAuto
+         control(5) = 1       ! iPrint = minimal level of output information
+         control(6) = 0       ! iErrMesgt: only critical termination allowed
+
+         Quality = 0.6
+
+         Call mbaNodal(
+c group (M)
+     &        nv, nvfix, nvmax, vrt, labelV, fixedV,
+     &        nb, nbfix, nbmax, bnd, labelB, fixedB,
+     &        nc,               crv, labelC, ANI_CrvFunction,
+     &        nt, ntfix, ntmax, tri, labelT, fixedT,
+c group (CONTROL)
+     &        nEStar, Quality, control, Metric,
+c group (W)
+     &        MaxWr, MaxWi, rW, iW, iERR)
+
+         If(iERR.GT.1000) Call errMesMBA(iERR, 'main',
+     &                        'unspecified error if mbaNodal')
+
+ 500     Continue
+      End do
+
+
+c === testing the results
+      If(Quality.LT.0.5) Stop 911
+      Stop 
+
+
+c error messages
+ 5000 Format(
+     & 'Program generates a mesh using the advanced front technique.',/,
+     & '  The initial front has', I4, ' edges.',/,
+     & '  The final mesh has', I4, ' triangles and', I4, ' vertices.',/,
+     & 'Created Postscript figure $(ANIHOME)/bin/mesh_final.ps.',/)
+ 5001 Continue
+      Write(*,*) 'Error occurred in umf4sym: ', luinfo(1)
+      Stop 911
+
+ 5002 Continue
+      Write(*,*) 'Error occurred in umf4num: ', luinfo(1)
+      Stop 911
+
+ 5003 Continue
+      Write(*,*) 'Error occurred in umf4sol: ', luinfo(1)
+      Stop 911
+
+      End
+
