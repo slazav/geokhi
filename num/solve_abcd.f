5b4904c082cd0a25ad075ebe7f62a5dbbd172242
Vladislav Zavjalov <slazav@altlinux.org>
2012-03-27 16:42:37 +0400

    abcd: rewrite mitrix ops

--- num/solve_abcd.f
+++ num/solve_abcd.f
@@ -1,14 +1,49 @@
-      Subroutine mmul(A1,B1,C1,D1, A2,B2,C2,D2)
-        real*8 A1,B1,C1,D1, A2,B2,C2,D2, A,B,C,D
-        A = A1 * A2 + B1 * C2
-        B = A1 * B2 + B1 * D2
-        C = C1 * A2 + D1 * C2
-        D = C1 * B2 + D1 * D2
-        A2 = A
-        B2 = B
-        C2 = C
-        D2 = D
+      Subroutine mmul(A, B)
+        real*8 A(2,2), B(2,2), C(2,2)
+        do i=1,2
+          do j=1,2
+            C(i,j) = A(1,j) * B(i,1) + A(2,j) * B(i,2)
+          enddo
+        enddo
+        do i=1,2
+          do j=1,2
+            B(i,j) = C(i,j)
+          enddo
+        enddo
       end
+C ======================================================================
+      subroutine m_space(A, L, N0)
+        real*8 A(2,2), L, N0
+        A(1,1)=1D0
+        A(2,1)=L/N0
+        A(1,2)=0D0
+        A(2,2)=1D0
+      end
+C ======================================================================
+      subroutine m_lens(A, F)
+        real*8 A(2,2), F
+        A(1,1)=1D0
+        A(2,1)=0D0
+        A(1,2)=-1D0/F
+        A(2,2)=1D0
+      end
+C ======================================================================
+      subroutine m_quad(A, L, N0, A0) ! N = N0(1 + A0 R^2 / 2)
+        real*8 A(2,2), L, N0, A0
+        A(1,1)=1D0 + A0 * L**2
+        A(2,1)=L/N0
+        A(1,2)=N0 * A0 * L
+        A(2,2)=1D0
+      end
+C ======================================================================
+      function do_abcd(q, A)
+        complex*16 q, do_abcd
+        real*8 A(2,2)
+        do_abcd = (A(1,1) * q + A(2,1))/(A(1,2) * q + A(2,2))
+      end
+C ======================================================================
+
+
 
 C ======================================================================
       Subroutine solve_ABCD(mx, nx,ny)
@@ -17,16 +52,14 @@ C ======================================================================
         integer i,j, nx,ny
         real*8 mx,y, N0, A0, E
         real*8 N, A2,  dy
-        real*8 AA,BB,CC,DD
+        real*8 A(2,2), DA(2,2)
         complex*16 Qi, Qo
+        complex*16 do_abcd
 
         open (57, FILE='dat/fit_n.dat')
         write (57,*) '## z N0 A0 ERR'
 
-        AA=1D0
-        BB=DIM_Li
-        CC=0D0
-        DD=1D0
+        call m_space(A, DIM_Li, 1D0)
 
         do i= -ny, ny
           dy = (DIM_L - 2*DIM_Lw)/ny/2.0
@@ -40,45 +73,40 @@ c         we have PHYS_N0 + N0 - A0 r^2
 
           write (57,*) y*i/abs(i), N, A2, E
 
-
-          call mmul(1D0 + A2 * dy**2, dy/N, N * A2 * dy, 1D0,
-     c              AA, BB, CC, DD)
+          call m_quad(DA, dy, N, A2)
+          call mmul(DA, A)
         enddo
         close(57)
 
-        call mmul(1D0, DIM_Lo, 0D0, 1D0,  AA, BB, CC, DD)
+        call m_space(DA, DIM_Lo, 1D0)
+        call mmul(DA, A)
 
 C        write(*,*) '>>>',AA,BB,CC,DD
         Qi=1D0/CMPLX(0D0, RAY_L/PI/RAY_W0**2)
-        Qo=(AA*Qi+BB)/(CC*Qi+DD)
-
-
+        Qo=do_abcd(Qi, A)
 
         open (57, FILE='dat/fit_abcd.dat')
-        write(57,*) AA,BB,CC,DD
+        write(57,*) A(1,1),A(2,1),A(1,2),A(2,2)
 
         write(*,*) 'R= ', 1D0/REAL(1D0/Qo), ' m'
         write(*,*) 'W= ', DSQRT(1D0/AIMAG(1D0/Qo) * RAY_L / PI)
      &    * 1000, ' mm'
 
 
-        write(57,*) AA,BB,CC,DD
+        write(57,*) A(1,1),A(2,1),A(1,2),A(2,2)
         close(57)
 
-        AA=1D0
-        BB=DIM_Li
-        CC=0D0
-        DD=1D0
-        call mmul(1D0, (DIM_L - 2D0 * DIM_Lw)/PHYS_N0,
-     &                  0D0, 1D0,  AA, BB, CC, DD)
-        call mmul(1D0, DIM_Lo, 0D0, 1D0,  AA, BB, CC, DD)
+        call m_space(A, DIM_Li, 1D0)
+        call m_space(DA, DIM_L - 2D0 * DIM_Lw, PHYS_N0)
+        call mmul(DA, A)
+        call m_space(DA, DIM_Lo, 1D0)
+        call mmul(DA, A)
 
 c        write(*,*) '>>>',AA,BB,CC,DD
-
 c        write(*,*) 'Q>>>', Qi, ' ', Qo
 
         Qi=1D0/CMPLX(0D0, RAY_L/PI/RAY_W0**2)
-        Qo=(AA*Qi+BB)/(CC*Qi+DD)
+        Qo=do_abcd(Qi, A)
 
 c        write(*,*) 'Q>>>', Qi, ' ', Qo
 
